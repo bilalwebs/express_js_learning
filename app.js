b57ef90ecb36a3104a546bb013f9ba7c4b3d50d7
bilalwebs <bil2023aljutt@gmail.com>
2026-06-27 14:45:23 +0500

    Implement MongoDB CRUD with Express.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js --> mongodb
 
 import express from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const dbName = "school";
 const url = "mongodb://localhost:27017";
@@ -41,6 +41,7 @@ const client = new MongoClient(url);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("view engine", "ejs");
 client.connect().then((connection) => {
   const db = connection.db(dbName);
@@ -60,16 +61,7 @@ client.connect().then((connection) => {
   });
 
   app.get("/add", async (req, resp) => {
-    resp.send(`
-        <form action="/add-student" method="post">
-      <input type="text" placeholder="enter user name" name="name" />
-      <br /><br />
-      <input type="text" placeholder="enter email" name="email" />
-      <br /><br />
-      <input type="text" placeholder="enter age" name="age" />
-      <br /><br />
-      <button type="">Submit</button>
-    </form>`);
+    resp.render("add-student");
   });
 
   app.post("/add-student", async (req, resp) => {
@@ -80,6 +72,82 @@ client.connect().then((connection) => {
     const result = await collection.insertOne(req.body);
 
     resp.send(result);
+  });
+
+  app.post("/add-student-api", async (req, resp) => {
+    console.log(req.body);
+
+    const { name, age, email } = req.body;
+    if (!name || !age || !email) {
+      resp.send({ message: "operation fail", success: false });
+      return false;
+    }
+    const collection = db.collection("students");
+
+    const result = await collection.insertOne(req.body);
+    resp.send({ message: "data stored", success: true, result: result });
+  });
+
+  app.get("/ui/delete/:id", async (req, resp) => {
+    const id = req.params.id;
+
+    const collection = db.collection("students");
+
+    const result = await collection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (result.deletedCount > 0) {
+      return resp.redirect("/ui");
+    }
+
+    resp.send("Student not found!");
+  });
+
+  app.get("/ui/student/:id", async (req, resp) => {
+    const id = req.params.id;
+    console.log(id);
+    const collection = db.collection("students");
+    const result = await collection.findOne({ _id: new ObjectId(id) });
+    resp.render("update-student", { result });
+  });
+
+  app.get("/student/:id", async (req, resp) => {
+    const id = req.params.id;
+    console.log(id);
+    const collection = db.collection("students");
+    const result = await collection.findOne({ _id: new ObjectId(id) });
+    resp.send({
+      message: "data fetch",
+      success: true,
+      result: result,
+    });
+  });
+
+  app.put("/update/:id", async (req, resp) => {
+    console.log(req.body);
+    console.log(req.params.id);
+
+    const collection = await db.collection("students");
+    const filter = { _id: new ObjectId(req.params.id) };
+    const update = {
+      $set: req.body,
+    };
+    const result = await collection.updateOne(filter, update);
+
+    if (result.modifiedCount > 0) {
+      resp.send({
+        message: "data Succesffuly",
+        success: true,
+        result: result,
+      });
+    } else {
+      resp.send({
+        message: "data not update",
+        success: false,
+        result: result,
+      });
+    }
   });
 });
 
